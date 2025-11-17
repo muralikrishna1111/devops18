46637942ed5dd6ac68f71da438515842bda413e1
muralikrishna1111 <muralike1234@gmail.com>
2025-11-17 23:01:29 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "one" {
-  bucket = "murali.devops"
+  bucket = "murali-monolithic-bucket"
 }
 
 resource "aws_s3_bucket_ownership_controls" "two" {
@@ -25,7 +25,7 @@ status = "Enabled"
 terraform {
 backend "s3" {
 region = "ap-south-1"
-bucket = "murali.devops"
+bucket = "murali-monolithic-bucket"
 key = "prod/terraform.tfstate"
 }
 }
